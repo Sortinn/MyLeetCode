1f7a25b888ab490079a24ade3eb39e21e678f57e
Sortinn <sortinndecar@gmail.com>
2018-04-03 21:51:37 +0800

    Finish 反转链表-迭代法

--- src/Problems/ReverseList.java
+++ src/Problems/ReverseList.java
@@ -1,7 +1,41 @@
 package Problems;
 
 public class ReverseList {
-    public void reverseList(ListNode head) {
+    public ListNode reverseList(ListNode head) {
+//        /**
+//         * 递归法
+//         */
+//        if (head == null || head.next == null) {
+//            return head;
+//        }
+//        ListNode p1 = head;
+//        ListNode p2 = head.next;
+//        while (p2 != null) {
+//            ListNode temp = p2.next;
+//            p2.next = p1;
+//            p1 = p2;
+//            p2 = temp;
+//        }
+//        head.next = null;
+//        return head;
+//    }
 
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p1 = head.next;
+        ListNode p2;
+        while (p1.next != null) {
+            p2 = p1.next;
+            p1.next = p2.next;
+            p2.next = head.next;
+            head.next = p2;
+        }
+        p1.next = head;
+        head = p1.next.next;
+        p1.next.next = null;
+
+        return head;
     }
 }
